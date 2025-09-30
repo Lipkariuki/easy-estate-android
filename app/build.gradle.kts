c0361cdb0b0c1afe19a2defa0302a28c76c49a73
Philip Theuri <Lipkariuki@users.noreply.github.com>
2025-09-30 20:29:29 +0300

    fix: add appcompat dependency for themes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui")
