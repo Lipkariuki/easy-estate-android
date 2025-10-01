51a53be9b5a4ed4fabf6babf5e8ce4fe4b649a35
Philip Kariuki <55229513+Lipkariuki@users.noreply.github.com>
2025-10-01 21:43:25 +0300

    Update weight imports for Compose scopes

--- app/src/main/java/com/easyestate/android/ui/LoginScreen.kt
+++ app/src/main/java/com/easyestate/android/ui/LoginScreen.kt
@@ -10,7 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.ColumnScope.weight
+import androidx.compose.foundation.layout.RowScope.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
