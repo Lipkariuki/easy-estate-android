80b3c856f43e94c488d5f79e40dbb9ccf3b39bb1
Philip Kariuki <55229513+Lipkariuki@users.noreply.github.com>
2025-10-01 21:20:18 +0300

    Add foundation layout dependency for Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.foundation:foundation-layout")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
