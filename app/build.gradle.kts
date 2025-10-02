b80c421217f387f908a98c376125cb083ed9a014
Philip Kariuki <55229513+Lipkariuki@users.noreply.github.com>
2025-10-02 10:32:01 +0300

    Add Material icons extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.foundation:foundation-layout")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
