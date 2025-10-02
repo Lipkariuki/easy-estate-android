92c3e3b7de26e3228c0e7466d7fb962e57f7a0e6
Philip Theuri <lipkariuki@gmail.com>
2025-10-02 18:02:41 +0300

    landing page changes

--- app/src/main/java/com/easyestate/android/ui/LandingScreen.kt
+++ app/src/main/java/com/easyestate/android/ui/LandingScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -28,8 +26,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.easyestate.android.R
 import com.easyestate.android.ui.theme.EasyEstateTheme
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Villa
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
