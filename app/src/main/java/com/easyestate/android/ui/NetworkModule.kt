19ac6de6310db5433cdef0fdeedf930c4429bc06
Philip Theuri <lipkariuki@gmail.com>
2025-10-02 22:59:22 +0300

    add unit

---
+++ app/src/main/java/com/easyestate/android/ui/NetworkModule.kt
@@ -0,0 +1,73 @@
+package com.easyestate.android.data
+
+import com.google.gson.annotations.SerializedName
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+// --- Data Transfer Objects (DTOs) ---
+
+data class AuthRequest(
+    val name: String? = null,
+    val email: String,
+    val password: String
+)
+
+data class Token(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("token_type") val tokenType: String
+)
+
+data class User(
+    val id: Int,
+    val email: String,
+    @SerializedName("is_active") val isActive: Boolean
+)
+
+// --- Retrofit API Service ---
+
+interface EasyEstateApiService {
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field("username") email: String,
+        @Field("password") password: String
+    ): Token
+
+    @POST("users/")
+    suspend fun register(@Body request: AuthRequest): User
+
+    @GET("users/me")
+    suspend fun getCurrentUser(@Header("Authorization") token: String): User
+}
+
+// --- Retrofit Client ---
+
+object ApiClient {
+    // Use 10.0.2.2 to connect to localhost from the Android emulator
+    private const val BASE_URL = "http://10.0.2.2:8000/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val instance: EasyEstateApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(EasyEstateApiService::class.java)
+    }
+}
