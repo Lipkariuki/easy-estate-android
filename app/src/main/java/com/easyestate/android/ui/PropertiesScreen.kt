b6abdb8f5815a477eee9c945c8348a0b8d861e43
Philip Theuri <lipkariuki@gmail.com>
2025-10-02 22:57:24 +0300

    add unit

--- app/src/main/java/com/easyestate/android/ui/PropertiesScreen.kt
+++ app/src/main/java/com/easyestate/android/ui/PropertiesScreen.kt
@@ -190,7 +190,8 @@ private fun QuickActionsSection(
                 PropertyActionTile(
                     action = action, onClick = {
                         when (action.title) {
-                            "Add Property" -> onNavigate(AppDestination.AddProperty.route) "Add Unit" -> onNavigate(AppDestination.AddUnit.route)
+                            "Add Property" -> onNavigate(AppDestination.AddProperty.route)
+                            "Add Unit" -> onNavigate(AppDestination.AddUnit.route)
                             else -> { /* TODO: Handle other actions */ }
                         }
                     },
